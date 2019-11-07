b25f8f069e7681ee5f1726cd0667b90725401b55
Aaron Cox <aaron@greymass.com>
2019-11-07 10:29:07 -0500

    +editorconfig and fix spacing

--- main.go
+++ main.go
@@ -12,25 +12,25 @@ import (
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
 type chainMapping struct {
-	endpoints map[string]string
+    endpoints map[string]string
 }
 
 var chainMap = chainMapping{
-  endpoints: make(map[string]string),
+    endpoints: make(map[string]string),
 }
 
 func (chainMap *chainMapping) createMap() {
-	chainMap.endpoints["aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906"] = "https://eos.greymass.com"
+    chainMap.endpoints["aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906"] = "https://eos.greymass.com"
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
     if host, ok := chainMap.endpoints[r.Header.Get("X-Chain-Id")]; ok {
-      url, _ := url.Parse(host)
-    	proxy := httputil.NewSingleHostReverseProxy(url)
-      r.URL.Host = url.Host
-    	r.URL.Scheme = url.Scheme
-    	r.Host = url.Host
-      proxy.ServeHTTP(w, r)
+        url, _ := url.Parse(host)
+        proxy := httputil.NewSingleHostReverseProxy(url)
+        r.URL.Host = url.Host
+        r.URL.Scheme = url.Scheme
+        r.Host = url.Host
+        proxy.ServeHTTP(w, r)
     }
 }
 
